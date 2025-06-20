ffed201b12fd1d6aa7bc0b65a5fab954337e728c
jakobtfaber <jakobtfaber@gmail.com>
2025-06-20 00:05:07 +0000

    moved files

--- scintillation/scint_analysis/scint_analysis/__init__.py
+++
@@ -1,8 +0,0 @@
-# ==============================================================================
-# File: scint_analysis/scint_analysis/__init__.py
-# ==============================================================================
-# This makes the core classes and main pipeline controller easily importable.
-
-from .core import DynamicSpectrum, ACF
-from .config import load_config
-from .pipeline import ScintillationAnalysis
